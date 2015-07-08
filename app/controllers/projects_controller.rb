729b9a4cd5ad4de750f46c90fa1a4a203bd0cfdb
mparaiso <mparaiso@online.fr>
2015-07-08 11:38:39 +0000

    4.4 What happens when things can't be found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,7 +12,7 @@ class ProjectsController < ApplicationController
     @project = Project.new project_params
     if @project.save
       #@note @rails combining redict and flash messages
-      redirect_to @project,notice:"Project has been created."
+      redirect_to @project, notice: "Project has been created."
     else
       flash[:alert] = "Project has not been created"
       render :new
@@ -23,10 +23,31 @@ class ProjectsController < ApplicationController
     @project = Project.find params[:id]
   end
 
+  def edit
+    @project = Project.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:id]
+    if @project.update project_params
+      flash[:notice] ='Project has been updated.'
+      redirect_to @project
+    else
+      flash[:alert]="Project has not been updated."
+      render :edit
+    end
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    flash[:notice] = "Project has been destroyed"
+    redirect_to projects_path
+  end
+
   private
 
   def project_params
-    params.require(:project).permit(:name,:description)
+    params.require(:project).permit(:name, :description)
   end
 
 end
